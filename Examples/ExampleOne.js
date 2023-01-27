e18d94eb92e834690dfcd1165b3345f9249d231a
Ronald Pe#a <ronald.micros@gmail.com>
2023-01-27 14:22:11 -0300

    JS_Example - Using var Variable

---
+++ Examples/ExampleOne.js
@@ -0,0 +1,12 @@
+// JavaScript - Example One (Use var)
+
+var variable = 5;
+// Scope
+{
+  console.log('inside', variable);
+  var variable = 10;
+}
+
+console.log('outside', variable);
+variable = variable * 2;
+console.log('changed', variable);
