4c8e3563b1d21aa680d798acdbfb905e40256d1f
Ronald Pe#a <ronald.micros@gmail.com>
2023-02-04 12:32:09 -0300

    JS_Example - Using const Variable

---
+++ Examples/Variables Names & Ambit/Example_3.js
@@ -0,0 +1,5 @@
+const variable = 5;
+  variable = variable * 2; // Error
+  console.log('changed', variable); // does't get here
+
+  /*This example has several errors, because the const variable can't change*/
