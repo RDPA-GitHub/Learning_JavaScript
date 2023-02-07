bf0563542738e73bde15777438d778f1f2a2cc4f
Ronald Pe#a <ronald.micros@gmail.com>
2023-02-06 23:59:15 -0300

    JS_Example - Correct use const

---
+++ Examples/Variables Names & Ambit/Example_5.js
@@ -0,0 +1,18 @@
+// JavaScript - Example 5 (Use Boolean)
+
+// bad
+const open = true;
+const write = true;
+const fruit = true;
+
+// good
+const isOpen = true;
+const canWrite = true;
+const hasFruit = true;
+
+console.log("open " + typeof(open));
+console.log("write " + typeof(write));
+console.log("fruit " + typeof(fruit));
+console.log("isOpen " + typeof(isOpen));
+console.log("canWrite " + typeof(canWrite));
+console.log("hasFruit " + typeof(hasFruit));
