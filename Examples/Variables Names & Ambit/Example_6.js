c831e101da3213afb4bc4e7d51fb76a27ee7dba9
Ronald Pe#a <ronald.micros@gmail.com>
2023-02-07 00:10:14 -0300

    JS_Example - Using Number

---
+++ Examples/Variables Names & Ambit/Example_6.js
@@ -0,0 +1,14 @@
+// JavaScript - Example 5 (Use Number)
+
+// Bad 
+const fruits = 3;
+
+// Better
+const maxFruits = 5;
+const minFruits = 1;
+const totalFruits = 3;
+
+console.log("fruits: " + fruits);
+console.log("maxFruits: " + maxFruits);
+console.log("minFruits: " + minFruits);
+console.log("totalFruits: " + totalFruits);
